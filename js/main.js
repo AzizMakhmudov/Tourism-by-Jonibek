0865348241f2bc31aa8261f6c03ba3b17ecbe940
AzizMakhmudov <azizmakhmudov2412@gmail.com>
2022-10-12 01:19:37 +0500

    commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+alert("salom")
+var money = Number(prompt("Qancha puliz bor?"))
+var Heading = document.querySelector(".heading")
+if (money >= 9973645.20) {
+    Heading.textContent = "Oq yo'l, Jonibek"
+}else {
+    Heading.textContent = "Jonibek, Shahzoddan so'ra, balki berar, u ham bermasa bollardan so'ra, Ular ham bermasa uyda telefonda kino ko'r"
+}
